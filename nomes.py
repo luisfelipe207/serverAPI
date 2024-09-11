7e2ded966edae1d3f188b612bb5fcb2cd13af738
Luis Felipe Ferreira <162385788+luisfelipe207@users.noreply.github.com>
2024-09-11 22:29:43 +0000

    desafios

--- nomes.py
+++ nomes.py
@@ -3,5 +3,7 @@
             "latitude": "180",
             "longitude": "120",
             "temperature": "20",
-            "interest": "nenhum"
+            "interest": "nenhum",
+            "email": "emailaleatorio@gmail.com",
+            "telephone": "(51)99999-9999"
         }
--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ from models import Lead, db
 
 # Função para gerar leads fictícios
 def generate_leads():
+    email = ['emailaleotorio@gmail.com', 'jjashdjajsadhb@gmail.com', 'hhahahaha@gmail.com']
     names = ['John Doe', 'Jane Smith', 'Chris Johnson', 'Patricia Brown', 'Michael Williams']
     interests = ['Tecnologia', 'Saúde', 'Educação', 'Marketing', 'Design']
+    telephone = ['(51)99999-9999', '(51)88888-8888','(51)77777-7777','(51)66666-6666']
 
     for _ in range(100):
         name = random.choice(names)
@@ -12,6 +14,9 @@ def generate_leads():
         longitude = random.uniform(-180, 180)
         temperature = random.uniform(10, 40)
         interest = random.choice(interests)
+        email = random.choice(email)
+        telephone = random.choice(telephone)
+        
         lead = Lead(name, latitude, longitude, temperature, interest)
         db.session.add(lead)
         db.session.commit()
